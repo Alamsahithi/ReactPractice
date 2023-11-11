023474d65373d962b248e4ee34d440a5b35dd3ee
Alamsahithi <sahithialam13@gmail.com>
2023-11-11 16:44:09 +0530

    outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
-    return <h2>Expenseitem list</h2>
+    const title = 'car'
+    return(
+        <div className='expense-item'>
+      <div>
+        Date
+      </div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>amount</div>
+      </div>
+    </div>
+    )
 }
 
 export default ExpenseItem
