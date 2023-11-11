5d0abc4a1308e895f9f4885afeeb353493dd9fbd
Alamsahithi <sahithialam13@gmail.com>
2023-11-11 21:09:47 +0530

    using jsx

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import ExpenseItem from "./components/ExpenseItem"
 function App(){
   const expenses =[
@@ -6,14 +7,20 @@ function App(){
     { title:'Car Insurance',amount:5000 ,date:new Date(2023,2,28)},
     { title:'Car Insurance',amount:5000 ,date:new Date(2023,2,28)},
   ]
-  return (
-    <div>
-      <h2>let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-    </div>
+  return React.createElement(
+    'div',
+    {},
+   React.createElement('div',{},"lrts get started"),
+   React.createElement(expenses,{items:expenses})
   )
+  // return (
+  //   <div>
+  //     <h2>let's get started</h2>
+  //     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+  //     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+  //     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+  //   </div>
+  // )
 }
 
 export default App
