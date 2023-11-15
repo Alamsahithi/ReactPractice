6d520897f3f72351dd28355cf126d42aab64f6ab
Alamsahithi <sahithialam13@gmail.com>
2023-11-15 21:17:45 +0530

    working with useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ExpenseItem from "./components/expenses/ExpenseItem"
+import Expenses from "../components/expenses/Expenses"
 function App(){
   const expenses =[
     { title:'Car Insurance',amount:5000 ,date:new Date(2023,2,28)},
@@ -7,20 +7,13 @@ function App(){
     { title:'Car Insurance',amount:5000 ,date:new Date(2023,2,28)},
     { title:'Car Insurance',amount:5000 ,date:new Date(2023,2,28)},
   ]
-  return React.createElement(
-    'div',
-    {},
-   React.createElement('div',{},"lrts get started"),
-   React.createElement(expenses,{items:expenses})
-  )
-  // return (
-  //   <div>
-  //     <h2>let's get started</h2>
-  //     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-  //     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-  //     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-  //   </div>
-  // )
+  
+ return (
+  <div>
+    <h2>lets get started</h2>
+    <Expenses items={expenses}/>
+  </div>
+ )
 }
 
 export default App
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,21 +1,23 @@
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props){
-  const month = props.date.toLocaleString('en-US', {month: 'long'})
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'})
-    const year = props.date.getFullYear()
+    const [title,setTitle] = useState(props.title)
+    
+    const clickHandler=() => {
+      setTitle('updated')
+      console.log(title)
+    }
     return(
     
         <Card className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+          <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
+     <button onClick={clickHandler}> change title</button>
     </Card>
     )
 }
